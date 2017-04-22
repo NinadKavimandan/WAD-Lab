221642db0f44c8f6e3ad7156e74a5bb1dc5dbf6d
NinadKavimandan <ninad.r.kavimandan@hotmail.com>
2017-04-22 09:11:49 +0530

    Add Existing Files

--- app.rb
+++ app.rb
@@ -1,8 +1,35 @@
 require 'sinatra'
 
 get '/' do
-	return 'Hello World'
+	name = params[:name]
+	if name!=nil
+		return "Hello #{name}!"
+	else 
+		return "No parameter!"
+	end
 end
-get '/inside' do
-	return 'Hi!'
+
+get '/Fibo' do
+	n = params[:n]
+	if n!=nil
+		i = n.to_i
+		num = Fib(i)
+		return "Value = #{num}"
+	else
+		return "No parameters given!"
+	end
+		
 end
+def Fib(n)
+	i=2
+	a=0
+	b=1
+	while i<n
+		c = a + b
+		a = b
+		b = c
+		i = i + 1
+	end
+	return c 
+end
+
